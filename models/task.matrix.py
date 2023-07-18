1ce2f56202316a7a0a07cddf4913c44c6637bb07
Teme <temeinem@gmail.com>
2023-07-18 10:37:33 +0300

    Update task.matrix.py

--- models/task.matrix.py
+++ models/task.matrix.py
@@ -1,1 +1,22 @@
+from odoo import models, fields
+
+class TaskMatrix(models.Model):
+    _name = 'task.matrix'
+    _description = 'Task Matrix'
+    
+    name = fields.Char('Name', required=True)
+    urgent = fields.Boolean('Urgent')
+    important = fields.Boolean('Important')
+    category = fields.Char('Category', compute='_compute_category')
+
+    def _compute_category(self):
+        for task in self:
+            if task.urgent and task.important:
+                task.category = 'Do'
+            elif not task.urgent and task.important:
+                task.category = 'Schedule'
+            elif task.urgent and not task.important:
+                task.category = 'Delegate'
+            else:
+                task.category = 'Eliminate'
 
